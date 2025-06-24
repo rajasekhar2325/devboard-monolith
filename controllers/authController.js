fe7235dc7e3454bce87de774ffb23ef099985997
rajasekhar <rajasekhar2325@gmail.com>
2025-06-24 15:11:51 +0000

    Add user auth with register/login endpoints

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hash });
+    res.status(201).json({ msg: 'User created' });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () => console.log(`Server on port ${process.env.PORT}`));
+  })
+  .catch(err => console.error(err));
