43f57bc93a2f3794bd9be0b1205e477e8edf67dd
rajasekhar <rajasekhar2325@gmail.com>
2025-06-28 06:08:52 +0000

    added minimal frontend

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const authRoutes = require('./routes/auth');
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+const path = require('path');
+app.use(express.static(path.join(__dirname)));
+
+const jwt = require('jsonwebtoken');
+
+app.get('/api/protected', (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).send('No token provided');
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.send(`Hello user ${decoded.id}`);
+  } catch (err) {
+    res.status(401).send('Invalid token');
+  }
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
